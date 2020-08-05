b14391e560f8abe145a5c11f80fef99ce83f7ea5
sunlubo <sunlubo.sun@gmail.com>
2020-08-05 17:43:17 +0800

    update Benchmarks

--- Sources/Benchmarks/main.swift
+++ Sources/Benchmarks/main.swift
@@ -9,35 +9,28 @@ import Foundation
 import Benchmark
 import JSON
 
-benchmark("parse json") {
-  // let source = try String(contentsOf: URL(string: "file:///Users/sun/Desktop/SwiftDOM/SwiftDOM/JSON.json")!)
-  let source = #"""
-     {
-       "string": "hello",
-       "int": 100,
-       "float": -3.14159265354,
-       "bool_true": true,
-       "bool_false": false,
-       "array": [1, 2, 3, 4, 5],
-       "null": null,
-       "object": {
-         "key": "value"
-       }
+let source = #"""
+   {
+     "string": "hello🍎👪",
+     "int": 100,
+     "float": -3.14159265354,
+     "bool_true": true,
+     "bool_false": false,
+     "array": [1, 2, 3, 4, 5],
+     "null": null,
+     "object": {
+       "key": "value"
      }
-    """#
-  // let source = "[]"
-  let _ = try JSON(string: source)
-  // let _ = try JSONSerialization.jsonObject(with: source.data(using: .utf8)!, options: .init())
-  // var printer = PrettyPrinter(json: json)
-  // printer.print()
+   }
+  """#
+
+let benchmarks = BenchmarkSuite(
+  name: "JSON Parsing", settings: Iterations(50000), WarmupIterations(1000)
+) { suite in
+
+  suite.benchmark("JSON") {
+    let _ = try JSON(string: source)
+  }
 }
 
-Benchmark.main()
-
-// name       time    std        iterations
-// ----------------------------------------
-// parse json 4510 ns ±  49.46 %     277708
-
-// name       time     std        iterations
-// -----------------------------------------
-// parse json 27353 ns ±  22.41 %      47774
+Benchmark.main([benchmarks])
